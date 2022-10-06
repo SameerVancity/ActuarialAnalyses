be423e1240abd79e66e3b7e53b8b8fccbfdba3b6
SameerVancity <76760818+SameerVancity@users.noreply.github.com>
2022-10-05 17:05:26 -0700

    Update SimDataV2.py

--- SimDataV2.py
+++ SimDataV2.py
@@ -8,18 +8,21 @@ from dateutil.relativedelta import relativedelta
 import sqlite3
 
 
-# ############Notes###########
+# ############ Notes ###########################################
 # Rule - Each member record has a memberID, DOB, Gender, LOB, incepDate, termDate
 # Rule - DOB values are restricted to a chosen range
 # Rule - Inception date values are restricted to a chosen range and greater than the DOB
-# Rule - Assume all policies are 1 year duration
+# Rule - Assume all policies terminate according to a daily geometric distribution
 # Rule - Assume all policy periods are continuous from inception until termination
 # Rule - Each loss record has a memberID, DOS, DOP, ProcCd, Billed, Allowed, Paid, Units, ClaimID, ClaimLine
 # Procedure - Want to simulate the member table first, and then simulate losses assigning each record to a memberID
 
+
+# ######### Step 1 - Define Functions ####################
+
 # function that randomly selects a given number of dates from within a given range with replacement and equal weight
 def randDate(minDate, maxDate, dateCount):
-    # Step 1 - Create the (immutable) tuple  of dates from which a date element
+    # Step 1.1 - Create the (immutable) tuple  of dates from which a date element
     # will be randomly selected (i.e. the sample space)
     dateSet = ()
     dateElement = minDate
@@ -27,14 +30,14 @@ def randDate(minDate, maxDate, dateCount):
         dateSet = dateSet + (dateElement,)
         dateElement = dateElement + timedelta(1)
 
-    # Step 2 - Create the set of weights to be applied to the set of dates above - for now, use uniform distribution
+    # Step 1.2 - Create the set of weights to be applied to the set of dates above - for now, use uniform distribution
     dateWeightSet = ([1]*len(dateSet))
 
-    # Step 3 - Randomly select from the dateSet
+    # Step 1.3 - Randomly select from the dateSet
     return random.choices(dateSet, dateWeightSet, cum_weights=None, k=dateCount)
 
 
-# #########Step 1 - Set parameters####################
+# ######### Step 2 - Set Member Data Parameters ####################
 # set the memberCount
 memberCount = 10000
 # set the DOB allowed range
@@ -42,35 +45,31 @@ dobFirst = date(1921, 1, 1)
 dobLast = date(2021, 12, 31)
 # set the LOB values
 lobValues = ('bronze', 'silver', 'gold')
-# set the inception date range and the probability of monthly renewal
+# set the inception date range and the daily probability of non-renewal
 inceptDateMin = date(2018, 1, 1)
 inceptDateMax = date(2021, 12, 31)
 termProb = 0.004  # daily probability of policy termination
 
-# #########Step 2 - Simulate Member data#####################
+# #########Step 3 - Simulate Member data #####################
 
-# 2.1 - simulate memberID as a 6 digit integer
+# 3.1 - simulate memberID as a 6 digit integer
 memberID = randint(100000, 999999, memberCount)
 
-# 2.2 - simulate DOB as on or after a given date (can enhance to build in the age distribution here)
+# 3.2 - simulate DOB as on or after a given date (can enhance to build in the age distribution here)
 # create a tuple (immutable) of DOB date values that will be randomly selected from
-
 dob = randDate(dobFirst, dobLast, memberCount)
-# print(dob)
 
-# 2.3 - Gender
+# 3.3 - Gender
 gender = np.random.choice(['M', 'F'], memberCount, replace=True)
-# print(gender)
 
-# 2.4 - LOB
+# 3.4 - LOB
 lob = np.random.choice(lobValues, memberCount, replace=True)
-# print(lob)
 
-# 2.5 - Inception Date
+# 3.5 - Inception Date
 randInceptDate = randDate(inceptDateMin, inceptDateMax, memberCount)
-InceptDate = np.maximum(dob, randInceptDate)
+inceptDate = np.maximum(dob, randInceptDate)
 
-# 2.5 - Termination Date - Simulate the number of months until termination as geometric
+# 3.6 - Termination Date - Simulate the number of months until termination as geometric
 policyDuration = np.random.geometric((termProb), memberCount)
 # create termDate - would be better to avoid looping here
 termDate = ()
@@ -80,5 +79,27 @@ for x in range(0, memberCount):
 
 # ############End Step ###########################################
 
+# #########Step 4 - Simulate loss data ####################
+# Rule - Assume a poisson frequency and log-normal severity distribution for loss by member
+# Simulate losses as a whole, and then randomly allocate those losses to the membership assuming a uniform distribution
 
+# 4.1 - Set the loss parameters
+
+lossLambda = 2.0
+lossMu = 3.0
+lossSigma = 3.0
+
+# 4.2 - Simulate the losses
+# each loss has a memberID, DOS, and Incurred Amount
+# for each member, simulate the number of losses and amount for each
+losses_memberID = np.arrray()
+losses_DOS = np.array()
+losses_Amount = np.array()
+
+for x in range(0, memberCount):
+    lossCount = random.poisson(lam=lossLambda*(termDate-inceptDate).days)
+    lossesIncurred = random.lognormal(lossMu, lossSigma, lossCount)
+    losses = losses + lossesIncurred
+
+#############################################################################################
 
